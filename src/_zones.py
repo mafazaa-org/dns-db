6caf5b48ed95e0bdbf488e3449a365982b75e887
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2024-08-19 02:48:51 +0300

    added cnames to zones

--- src/_zones.py
+++ src/_zones.py
@@ -2,7 +2,30 @@ low = [
     # safe search | strict mode
     {
         # google & youtube
-        "host": "(www\.google\..+)|(((www\.youtube(-nocookie)?)|(m\.youtube)|(youtubei?\.googleapis))\.com)",
+        "host": "(www\.google\..+)",
+        "answers": [
+            {"type": "CNAME", "answer": "forcesafesearch.google.com"},
+        ],
+    },
+    {
+        "host": "(((www\.youtube(-nocookie)?)|(m\.youtube)|(youtubei?\.googleapis))\.com)",
+        "answers": [{"type": "CNAME", "answer": "restrict.youtube.com"}],
+    },
+    {
+        "host": "forcesafesearch.google.com",
+        "answers": [
+            {
+                "type": "A",
+                "answer": "216.239.38.120",
+            },
+            {
+                "type": "AAAA",
+                "answer": "2a00:1450:4006:811::2004",
+            },
+        ],
+    },
+    {
+        "host": "restrict.youtube.com",
         "answers": [
             {
                 "type": "A",
